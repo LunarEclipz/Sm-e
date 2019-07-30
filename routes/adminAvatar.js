f23984065f7fc340b4464103a1e31db2cfe6e49b
LunarEclipz <40319463+LunarEclipz@users.noreply.github.com>
2019-07-31 00:43:11 +0800

    Add files via upload

---
+++ routes/adminAvatar.js
@@ -0,0 +1,318 @@
+const express = require('express');
+const router = express.Router();
+const Pet = require('../models/adminPet');
+const Aspect = require('../models/adminAvatar');
+const fs = require('fs');
+const upload = require('../helpers/imageUpload');
+const firstUpload = require('../helpers/firstLevel');
+const secondUpload = require('../helpers/secondLevel');
+const thirdUpload = require('../helpers/thirdLevel');
+
+
+
+
+// var Handlebars = require('handlebars');
+
+// Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+// 	if(v1 == v2) {
+// 	  return options.fn(this);
+// 	}
+// 	return options.inverse(this);
+//   });
+router.post('/upload', (req, res) => {
+	// Creates user id directory for upload if not exist
+	if (!fs.existsSync('./public/uploads/' )) {
+		fs.mkdirSync('./public/uploads/' );
+	}
+
+	upload(req, res, (err) => {
+		if (err) {
+			res.json({
+				file: '/img/no-image.jpg',
+				err: err
+			});
+		} else {
+			if (req.file === undefined) {
+				res.json({
+					file: '/img/no-image.jpg',
+					err: err
+				});
+			} else {
+				res.json({
+					file: `/uploads/${req.file.filename}`
+				});
+			}
+		}
+	});
+})
+
+router.post('/firstUpload', (req, res) => {
+	// Creates user id directory for upload if not exist
+	if (!fs.existsSync('./public/uploads/' )) {
+		fs.mkdirSync('./public/uploads/' );
+	}
+
+	firstUpload(req, res, (err) => {
+		if (err) {
+			res.json({
+				file: '/img/no-image.jpg',
+				err: err
+			});
+		} else {
+			if (req.file === undefined) {
+				res.json({
+					file: '/img/no-image.jpg',
+					err: err
+				});
+			} else {
+				res.json({
+					file: `/uploads/${req.file.filename}`
+				});
+			}
+		}
+	});
+})
+
+router.post('/secondUpload', (req, res) => {
+	// Creates user id directory for upload if not exist
+	if (!fs.existsSync('./public/uploads/' )) {
+		fs.mkdirSync('./public/uploads/' );
+	}
+
+	secondUpload(req, res, (err) => {
+		if (err) {
+			res.json({
+				file: '/img/no-image.jpg',
+				err: err
+			});
+		} else {
+			if (req.file === undefined) {
+				res.json({
+					file: '/img/no-image.jpg',
+					err: err
+				});
+			} else {
+				res.json({
+					file: `/uploads/${req.file.filename}`
+				});
+			}
+		}
+	});
+})
+
+router.post('/thirdUpload', (req, res) => {
+	// Creates user id directory for upload if not exist
+	if (!fs.existsSync('./public/uploads/' )) {
+		fs.mkdirSync('./public/uploads/' );
+	}
+
+	thirdUpload(req, res, (err) => {
+		if (err) {
+			res.json({
+				file: '/img/no-image.jpg',
+				err: err
+			});
+		} else {
+			if (req.file === undefined) {
+				res.json({
+					file: '/img/no-image.jpg',
+					err: err
+				});
+			} else {
+				res.json({
+					file: `/uploads/${req.file.filename}`
+				});
+			}
+		}
+	});
+})
+router.post('/addAspect', (req, res) => {
+	let Fields = req.body.Fields;
+	let Tasks = req.body.Tasks;
+	let noTasks = req.body.noTasks;
+	let Name = req.body.Name;
+	let Quote = req.body.Quote;
+	let firstURL = req.body.firstURL;
+	let Tasks2 = req.body.Tasks2;
+	let noTasks2 = req.body.noTasks2;
+	let Quote2 = req.body.Quote2;
+	let secondURL = req.body.secondURL;
+	let Tasks3 = req.body.Tasks3;
+	let noTasks3 = req.body.noTasks3;
+	let Quote3 = req.body.Quote3;
+	let thirdURL = req.body.thirdURL;
+	console.log(Fields, Tasks, Name, Quote, noTasks, Tasks2, Quote2, noTasks2, Tasks3, Quote3, noTasks3)
+	Aspect.create({
+		Fields,
+		Tasks,
+		Name,
+		Quote,
+		noTasks,
+		firstURL,
+		Tasks2,
+		Quote2,
+		noTasks2,
+		secondURL,
+		Tasks3,
+		Quote3,
+		noTasks3,
+		thirdURL
+	}).then((Aspect) => {
+		res.redirect('/adminAvatar/editAvatar');
+	})
+});
+
+router.post('/addPet', (req, res) => {
+	let petName = req.body.petName;
+	let tasks = req.body.tasks;
+	let noTasks = req.body.noTasks;
+	let quotes = req.body.quotes;
+	let petPicURL = req.body.petPicURL;
+
+	console.log(petName, tasks, quotes, noTasks, petPicURL)
+	Pet.create({
+		petName,
+		tasks,
+		quotes,
+		noTasks,
+		petPicURL,
+	}).then((Pet) => {
+		res.redirect('/adminAvatar/pets');
+	})
+});
+
+
+router.get('/', (req, res) => {
+	res.render('adminAvatar/adminAvatar') // renders views/index.handlebars
+});
+
+
+
+router.get('/pets', (req, res) => {
+	Pet.findAll({
+	}).then((Pet) => {
+		res.render('adminAvatar/pet', {
+			Pet: Pet
+		});
+	});
+});
+
+router.get('/petInfo/:id', (req, res) => {
+	Pet.findOne({
+		where: {
+			id: req.params.id
+		}
+	}).then((Pet) => {
+		res.render('adminAvatar/petInfo', {
+			Pet: Pet
+		});
+	});
+});
+
+router.get('/editAspect/:id', (req, res) => {
+	Aspect.findOne({
+		where: {
+			id: req.params.id
+		}
+	}).then((Aspect) => {
+		res.render('adminAvatar/editAspect', {
+			Aspect: Aspect
+		});
+	});
+});
+
+
+router.post('/updatePet/:id', (req, res) => {
+	var id = req.params.id
+	let petName = req.body.petName;
+	let tasks = req.body.tasks;
+	let noTasks = req.body.noTasks;
+	let quotes = req.body.quotes;
+	Pet.update({ petName: petName, tasks: tasks, noTasks: noTasks, quotes: quotes }, {
+		where: { id: id }
+	})
+		.then((Pet) => {
+			res.redirect('/adminAvatar/pets');
+		})
+});
+
+router.post('/updateAspect/:id', (req, res) => {
+	var id = req.params.id
+
+	let Fields = req.body.Fields;
+	let Tasks = req.body.Tasks;
+	let noTasks = req.body.noTasks;
+	let Name = req.body.Name;
+	let Quote = req.body.Quote;
+	let Tasks2 = req.body.Tasks2;
+	let noTasks2 = req.body.noTasks2;
+	let Quote2 = req.body.Quote2;
+	let Tasks3 = req.body.Tasks3;
+	let noTasks3 = req.body.noTasks3;
+	let Quote3 = req.body.Quote3;
+	Aspect.update({ Fields: Fields, Quote: Quote, Tasks: Tasks, noTasks: noTasks, Name: Name,
+		Quote2: Quote2, Tasks2: Tasks2, noTasks2: noTasks2, 
+		Quote3: Quote3, Tasks3: Tasks3, noTasks3: noTasks3  }, {
+		where: { id: id }
+	})
+		.then((Aspect) => {
+			res.redirect('/adminAvatar/editAvatar');
+		})
+});
+
+router.post('/deletePet/:id', (req, res) => {
+	var id = req.params.id
+	Pet.destroy({
+		where: {
+			id: id
+		}
+	}).then((Pet) => {
+		res.redirect('/adminAvatar/pets');
+	})
+});
+
+router.post('/deleteAspect/:id', (req, res) => {
+	var id = req.params.id
+	Aspect.destroy({
+		where: {
+			id: id
+		}
+	}).then((Aspect) => {
+		res.redirect('/adminAvatar/editAvatar');
+	})
+});
+
+
+
+
+// .catch(err => console.log(err))
+// res.render('adminAvatar/editPet') // renders views/index.handlebars
+
+
+router.get('/editPet', (req, res) => {
+
+	res.render('adminAvatar/editPet');
+
+})
+
+router.get('/addAspect', (req, res) => {
+
+	res.render('adminAvatar/addAspect');
+
+})
+// Upload poster
+
+router.get('/editAvatar', (req, res) => {
+	Aspect.findAll({
+	}).then((Aspect) => {
+		res.render('adminAvatar/editAvatar', {
+			Aspect:Aspect
+		}); // renders views/index.handlebars
+	})
+});
+
+
+
+
+
+module.exports = router;
--- routes/avatar.js
+++ routes/avatar.js
@@ -1,33 +1,315 @@
 const express = require('express');
 const router = express.Router();
+const Avatar = require('../models/avatar');
+const Pet = require('../models/adminPet');
+const Aspect = require('../models/adminAvatar');
+const Sequelize = require('sequelize');
+
+const Op = Sequelize.Op;
+
+
 
 router.get('/', (req, res) => {
-    const EmptyAvatar = 'src="/img/Empy_Mark.png" height=240 width=320';
-    const EmptyPet    = 'src="/img/Empy_Mark.png" height=240 width=320';
-	res.render('avatar/avatar', {AvatarPic: EmptyAvatar, PetPic: EmptyPet}) // renders views/index.handlebars
+	const EmptyAvatar = 'src="/img/Empy_Mark.png" height=240 width=320';
+	const EmptyPet = 'src="/img/Empy_Mark.png" height=240 width=320';
+	Avatar.findOne({
+		// 	where: { username: user },
+		// 	raw: true
+	}).then(check_user => {
+		const checkuser = check_user.username;
+
+
+		Avatar.findOne({
+			attributes: ['feelings'],
+			where: { username: checkuser },
+			raw: true
+		}).then(userfeelings => {
+			const user_feelings = userfeelings.feelings
+			Aspect.findAll({}, {
+				where: { Fields: 'Feelings', Name: user_feelings },
+				raw: true
+			}).then(feelings => {
+
+				Avatar.findOne({
+					attributes: ['thoughts'],
+					where: { username: checkuser },
+					raw: true
+				}).then(userthoughts => {
+
+					const user_thoughts = userthoughts.thoughts;
+
+
+					Aspect.findAll({
+						where: { Name: user_thoughts },
+						raw: true
+					}).then(thoughts => {
+
+
+
+						Avatar.findOne({
+							attributes: ['actions'],
+							where: { username: checkuser },
+							raw: true
+						}).then(useractions => {
+							const user_actions = useractions.actions;
+
+
+							Aspect.findAll({
+								where: { Name: user_actions },
+								raw: true
+							}).then(actions => {
+
+
+								Avatar.findOne({
+									attributes: ['aura'],
+									where: { username: checkuser },
+									raw: true
+								}).then(useraura => {
+									const user_aura = useraura.aura;
+
+
+									Aspect.findAll({
+										where: { Name: user_aura },
+										raw: true
+									}).then(aura => {
+										const fl = 1;
+										const tl = 1;
+										const al = 1;
+										const ul = 1;
+
+										Avatar.findOne({
+											attributes: ['pet'],
+											where: { username: checkuser },
+											raw: true
+										}).then(userpet => {
+											const user_pet = userpet.pet;
+
+
+											Pet.findAll({
+												where: { petName: user_pet },
+												raw: true
+											}).then(pets => {
+												console.log("HERE!!!!!!!!!", userpet);
+
+												console.log("HERE!!!!!!!!!", pets);
+												res.render('avatar/avatar', {
+													AvatarPic: EmptyAvatar, PetPic: EmptyPet,
+													checkuser: checkuser, feelings: feelings[0], thoughts: thoughts[0],
+													actions: actions[0], aura: aura[0], fl: fl, tl: tl, al: al, ul: ul, check_user: check_user,
+													pets: pets[0]
+												});
+											}).catch(err =>
+
+												res.render('avatar/avatar', {
+													AvatarPic: EmptyAvatar, PetPic: EmptyPet,
+													checkuser: checkuser, feelings: feelings[0], thoughts: thoughts[0],
+													actions: actions[0], aura: aura[0], fl: fl, tl: tl, al: al, ul: ul, check_user: check_user,
+												})); // To catch no video ID
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+
+		}).catch(err =>
+			Avatar.findOne({
+				attributes: ['pet'],
+				where: { username: checkuser },
+				raw: true
+			}).then(userpet => {
+				const user_pet = userpet.pet;
+
+
+				Pet.findAll({
+					where: { petName: user_pet },
+					raw: true
+				}).then(pets => {
+					res.render('avatar/avatar', {
+						AvatarPic: EmptyAvatar, PetPic: EmptyPet,
+						checkuser: checkuser, fl: fl, tl: tl, al: al, ul: ul, check_user: check_user,
+						pets: pets[0]
+					});
+				}).catch(err =>
+
+					res.render('avatar/avatar', {
+						AvatarPic: EmptyAvatar, PetPic: EmptyPet,
+						checkuser: checkuser, fl: fl, tl: tl, al: al, ul: ul, check_user: check_user,
+					})); // To catch no video ID
+			}));
+	}).catch(err =>
+		res.render('avatar/avatar', {
+			AvatarPic: EmptyAvatar, PetPic: EmptyPet,
+		}));
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// var Avatar = "SELECT * from smile.avatars";
+// var thoughts = "SELECT Fields,Tasks,Name,Quote,noTasks,firstURL,Tasks2,	Quote2,	noTasks2,secondURL,Tasks3,	Quote3,	noTasks3,thirdURL from smile.aspects a inner join smile.avatars s on a.name = s.thoughts;  ";
+
+// smile.query(Avatar, function (err, Avatar) {
+//   if (err) throw err;
+//   console.log(Avatar);
+// });
+
+// smile.query(thoughts, function (err, thoughts) {
+//   if (err) throw err;
+//   console.log(thoughts);
+// });
+
+
+
+// 		res.render('avatar/avatar', { AvatarPic: EmptyAvatar, PetPic: EmptyPet, Avatar: Avatar });
+//  })
+// renders views/index.handlebars
+
 
 router.get('/createAvatar', (req, res) => {
-	res.render('avatar/createAvatar') // renders views/index.handlebars
+
+	Pet.findAll({
+	}).then(pets => {
+		Aspect.findAll({
+		}).then((Aspect) => {
+		res.render('avatar/createAvatar', {
+			Aspect: Aspect,
+			pets: pets
+		});
+	}).catch(err =>
+
+		Aspect.findAll({
+		}).then((Aspect) => {
+			res.render('avatar/createAvatar', {
+				Aspect: Aspect,
+			}) // To catch no video ID
+		}));
+	});
 });
 
-router.get('/createPet', (req, res) => {
-	res.render('avatar/createPet') // renders views/index.handlebars
-});
-
-router.get('/deletePet', (req, res) => {
-	res.render('avatar/deletePet') // renders views/index.handlebars
-});
-
-router.get('/deleteAvatar', (req, res) => {
-	res.render('avatar/deleteAvatar') // renders views/index.handlebars
-});
 
 
-// Logout User
-router.get('/logout', (req, res) => {
-	req.logout();
-	res.redirect('/');
-});
 
-module.exports = router;
+
+
+
+	router.post('/updatePet', (req, res) => {
+		let feelingsPet = req.body.feelingsPet;
+		let pet = req.body.motiv;
+		console.log(feelingsPet, pet)
+		Avatar.update({ feelingsPet: feelingsPet, pet: pet }, {
+			where: { username: "tom" }
+		}).then((Avatar) => {
+			res.redirect('/avatar');
+		})
+	});
+
+	router.post('/updateAvatar', (req, res) => {
+		let username = "tom";
+		let thoughts = req.body.thoughts;
+		let feelings = req.body.feelings;
+		let aura = req.body.aura;
+		let actions = req.body.actions;
+		Avatar.update({ username: username, thoughts: thoughts, aura: aura, actions: actions, feelings: feelings }, {
+			where: { username: "tom" }
+		}).then((Avatar) => {
+			res.redirect('/avatar');
+		})
+	});
+
+	router.get('/createPet', (req, res) => {
+		Avatar.findOne({
+			where: { pet: { [Op.ne]: null } }
+		}).then(Avatar => {
+			Pet.findAll({
+				// where: { petName: user_pet },
+				// raw: true
+			}).then(pets => {
+				const petName = pets[0].petName;
+				console.log("PETTsdsdsdITTTS", petName)
+
+				res.render('avatar/createPet', {
+					pets: pets,
+					petName: petName,
+					Avatar: Avatar
+				});
+			});
+
+		}).catch(err =>
+			Pet.findAll({
+				// where: { petName: user_pet },
+				// raw: true
+			}).then(pets => {
+				const petName = pets[0].petName;
+
+				console.log("PETTITTTS", petName)
+				res.render('avatar/createPet', {
+					pets: pets,
+					petName: petName,
+					Avatar: Avatar
+				});
+			}));
+
+	});
+
+
+
+
+	router.get('/deletePet', (req, res) => {
+		res.render('avatar/deletePet') // renders views/index.handlebars
+	});
+
+	router.get('/deleteAvatar', (req, res) => {
+		res.render('avatar/deleteAvatar') // renders views/index.handlebars
+	});
+
+
+	// Logout User
+	router.get('/logout', (req, res) => {
+		req.logout();
+		res.redirect('/');
+	});
+
+	router.post('/saveAvatar', (req, res) => {
+		let username = "tom";
+		let thoughts = req.body.thoughts;
+		let feelings = req.body.feelings;
+		let aura = req.body.aura;
+		let actions = req.body.actions;
+		console.log(thoughts, feelings, aura, actions)
+		Avatar.create({
+			thoughts,
+			feelings,
+			aura,
+			actions,
+			username
+		}).then((Avatar) => {
+			res.redirect('/avatar');
+		})
+	});
+
+	router.post('/savePet', (req, res) => {
+		let username = "tom";
+		let feelingsPet = req.body.feelingsPet;
+		let pet = req.body.motiv;
+		Avatar.create({
+			feelingsPet,
+			pet,
+			username
+		}).then((Avatar) => {
+			res.redirect('/avatar');
+		})
+	});
+
+	module.exports = router;

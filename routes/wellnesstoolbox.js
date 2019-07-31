47f435806008ccafe092ef60a8ec1faab3aa380f
RyanzJr <ryantanhuiyang45aips11@gmail.com>
2019-07-31 14:13:20 +0800

    Add files via upload

--- routes/wellnesstoolbox.js
+++ routes/wellnesstoolbox.js
@@ -4,7 +4,22 @@ const alertMessage = require('../helpers/messenger');
 const article = require('../models/Article');
 const fs = require('fs');
 //const upload = require('../helpers/imageUpload');
-const multer = require('multer')
+const multer = require('multer');
+
+
+let today = new Date();
+let dd = today.getDate();
+
+let mm = today.getMonth() + 1;
+let yyyy = today.getFullYear();
+if (dd < 10) {
+	dd = '0' + dd;
+}
+
+if (mm < 10) {
+	mm = '0' + mm;
+}
+today = dd + '/' + mm + '/' + yyyy;
 
 const storage = multer.diskStorage({
 	destination: function (req, file, callback) {
@@ -13,7 +28,7 @@ const storage = multer.diskStorage({
 	filename: function (req, file, callback) {
 		callback(null, Date.now() + file.originalname);
 	}
-})
+});
 
 const fileFilter = (req, file, callback) => {
 	if (file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
@@ -31,13 +46,183 @@ const upload = multer({
 
 router.get('/user/wellnesstoolbox', (req, res) => {
 	const Title = 'Wellness Toolbox';
-	res.render('wellnesstoolbox/user/wellnesstoolbox', { Title: Title })
+
+	//Retrieve Data from DB
+	article.findAll({
+		raw: true
+	}).then((article) => {
+		sortArticle(article)
+		res.render('wellnesstoolbox/user/wellnesstoolbox', {
+			Title: Title,
+			medArticle,
+			vidArticle,
+			musicArticle,
+			workArticle,
+			depArticle
+		});
+	}).catch(err => console.log(err))
 });
 
-router.get('/admin/wtbmanagement', (req, res) => {
-	const Title = 'WTB Management';
 
-	//Retrieve Data
+
+//Create
+router.post('/admin/wtbmanagement', upload.single('Thumbnail'), (req, res) => {
+	//Get values from form
+	let category = req.body.Category;
+	let title = req.body.Title;
+	let description = req.body.Description;
+	let link = req.body.URL;
+	let errors = 0
+
+	req.flash('info', 'Flash is back!')
+ 	
+
+
+	if (title.length > 14) {
+		req.flash('title', 'Title must be 14 characters or less')
+	}
+
+	if (description.length > 30) {
+		req.flash('desc', 'Description must be 30 characters or less')
+	}
+
+	if (errors > 0) {
+		res.redirect('/wellnesstoolbox/admin/wtbmanagement')
+	}
+
+	else {
+
+		//If Have Thumbnail, Create
+		if (req.file != undefined) {
+			article.create({
+				category,
+				title,
+				description,
+				link,
+				thumbnail: ('/' + req.file.path),
+				date: today,
+				time: (new Date()).getTime(),
+				view: 0
+			}).then((article) => {
+
+				res.redirect('/wellnesstoolbox/admin/wtbmanagement')
+			})
+		}
+
+		//If No Thumbnail, Create with defaultThumbnail
+		else {
+			article.create({
+				category,
+				title,
+				description,
+				link,
+				thumbnail: '/uploads/default.jpg',
+				date: today,
+				time: (new Date()).getTime(),
+				view: 0
+			}).then((article) => {
+				res.redirect('/wellnesstoolbox/admin/wtbmanagement')
+			})
+		}
+
+	}
+
+	//Form Info
+	// console.log(req.body)
+	//Thumbnail Info
+	// console.log(req.file)
+	//Insert values into DB
+
+});
+//Delete
+router.get('/admin/wtbmanagement/delete/:id', (req, res) => {
+	article.findOne({
+		where: {
+			id: req.params.id,
+			//userId: req.user.id
+		},
+	}).then((articles) => {
+		article.destroy({
+			where: {
+				id: articles.id
+			}
+		}).then((article) => {
+			res.redirect('/wellnesstoolbox/admin/wtbmanagement')
+		})
+	})
+});
+
+//Edit
+router.post('/admin/wtbmanagement/edit/:id', upload.single('Thumbnail'), (req, res) => {
+	let newcategory = req.body.Category
+	let newtitle = req.body.Title
+	let newdescription = req.body.Description
+	let newlink = req.body.URL
+	console.log(`\x1b[33m cat: ${newcategory}, title: ${newtitle}, desc: ${newdescription}, link: ${newlink}\x1b[0m`);
+	console.log(`\x1b[33m thumbnail: ${req.file}\x1b[0m`);
+
+	article.findOne({
+		raw: true,
+		where: {
+			id: req.params.id,
+			//userId: req.user.id
+		}
+	}).then(data => {
+		if (data) {
+			//If have Thumbnail, Set new Thumbnail
+			if (req.file != undefined) {
+				article.update({
+					category: newcategory,
+					title: newtitle,
+					description: newdescription,
+					link: newlink,
+					thumbnail: ('/' + req.file.path),
+					date: today,
+					time: (new Date()).getTime()
+				},
+					{
+						where: { id: data['id'] }
+					}).then(() => {
+						res.redirect('/wellnesstoolbox/admin/wtbmanagement');
+					})
+			}
+			else {
+				//If No Thumbnail, Dont set Thumbnail
+				article.update({
+					category: newcategory,
+					title: newtitle,
+					description: newdescription,
+					link: newlink,
+					date: today,
+					time: (new Date()).getTime()
+				},
+					{
+						where: { id: data['id'] }
+					}).then(() => {
+						res.redirect('/wellnesstoolbox/admin/wtbmanagement');
+					})
+			}
+		}
+
+		else {
+			res.redirect('/wellnesstoolbox/admin/wtbmanagement');
+		}
+	});
+});
+
+//RENDER
+router.get('/admin/wtbmanagement', (req, res) => {
+	
+
+	const Title = 'WTB Management';
+	let errors = '';
+	
+	//let errors = [{text:req.flash('desc')}];
+
+
+
+
+	//Retrieve Data from DB
 	article.findAll({
 		raw: true
 	}).then((article) => {
@@ -48,7 +233,11 @@ router.get('/admin/wtbmanagement', (req, res) => {
 			vidArticle,
 			musicArticle,
 			workArticle,
-			depArticle
+			depArticle,
+			article,
+			errors: errors,
+			test: req.flash('info')
+
 		});
 	}).catch(err => console.log(err))
 });
@@ -72,33 +261,6 @@ function sortArticle(article) {
 			depArticle.push(article[i])
 		}
 	}
-}
-
-router.post('/admin/wtbmanagement', upload.single('Thumbnail'), (req, res) => {
-	//Get values from form
-	let category = req.body.Category
-	let title = req.body.Title
-	let description = req.body.Description
-	let link = req.body.URL
-	let thumbnail = req.file.path
-	//console.log(req.body)
-
-	if(title === '') {
-		
-	}
-
-
-
-	//Insert values into DB
-	article.create({
-		category,
-		title,
-		description,
-		link,
-		thumbnail
-	}).then((article) => {
-		res.redirect('/wellnesstoolbox/admin/wtbmanagement')
-	})
-});
+};
 
 module.exports = router;

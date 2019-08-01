e8fe8cabbc2732bb8fae1042bffe912dc3a839eb
Starlightier <182394X@mymail.nyp.edu.sg>
2019-08-01 11:23:55 +0800

    Add files via upload

--- routes/journal.js
+++ routes/journal.js
@@ -2,23 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Journal = require('../models/Journal');
 
-
-router.get('/', (req, res) => {
-	const title = 'Video Jotter';
-	res.render('index', { title: title }) // renders views/index.handlebars
-});
-
-// Logout User
-router.get('/logout', (req, res) => {
-	req.logout();
-	res.redirect('/');
-});
-
 // Calendar
 router.get('/calendar', (req, res) => {
 	Journal.findAll({
 	}).then((Journal) => {
-		res.render('user/calendar', {
+		res.render('journal/calendar', {
 			Journal: Journal
 		});
 	});
@@ -27,13 +15,13 @@ router.get('/calendar', (req, res) => {
 // Mood Tracker
 router.get('/moodTracker', (req, res) => {
 	const Title = "Mood Tracker";
-	res.render('user/moodTracker', { title: Title })
+	res.render('journal/moodTracker', { title: Title })
 });
 
 // New Entry
 router.get('/newEntry', (req, res) => {
 	const Title = "New Entry";
-	res.render('user/newEntry', { title: Title })
+	res.render('journal/newEntry', { title: Title })
 });
 
 // Save Entry
@@ -42,55 +30,58 @@ router.post('/saveEntry', (req, res) => {
 	let mood = req.body.mood;
 	let emotion = req.body.emotion;
 	let date = req.body.date;
-	console.log(entry, date, mood, emotion)
+	let title = req.body.title;
+	console.log(entry, date, mood, emotion, title)
 
 	Journal.create({
 		entry,
 		mood,
 		emotion,
-		date
+		date,
+		title
 	}).then((Journal) => {
-		res.redirect('/calendar');
+		res.redirect('/journal/calendar');
 	})
 });
 
 // Update Entry
 router.get('/updateEntry/:id', (req, res) => {
 	Journal.findOne({
-		where:{
+		where: {
 			id: req.params.id
 		}
-	}).then((Journal) => {
-		res.render('user/updateEntry', {
-			Journal: Journal
+	}).then((journal) => {
+		res.render('journal/updateEntry', {
+			journal: journal
 		});
 	});
 });
 
 router.post('/updateEntry/:id', (req, res) => {
-	var id = req.params.id
+	let id = req.params.id;
 	let entry = req.body.entry;
 	let mood = req.body.mood;
 	let emotion = req.body.emotion;
 	let date = req.body.date;
-	Journal.update({entry: entry, date: date, mood: mood, emotion: emotion}, {
-		where: {id: id}
-		})
-		.then((Journal) => {
-		res.redirect('/calendar');
+	let title = req.body.title;
+	Journal.update({ entry: entry, date: date, mood: mood, emotion: emotion, title: title }, {
+		where: { id: id }
 	})
+		.then((Journal) => {
+			res.redirect('/journal/calendar');
+		})
 
 });
 
 // Delete Entry
 router.post('/deleteEntry/:id', (req, res) => {
-	var id = req.params.id
+	let id = req.params.id
 	Journal.destroy({
 		where: {
 			id: id
 		}
 	}).then((Journal) => {
-		res.redirect('/calendar');
+		res.redirect('/journal/calendar');
 	})
 });
 

f9daf2263b88ea174dcbfdbf410d8637192f886c
Aadi <44631042+AadiRawr@users.noreply.github.com>
2019-08-05 01:40:30 +0800

    Update hbs.js

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,3 +1,10 @@
+const path = require('path')
+const fs = require('fs')
+const moment = require('moment');
+
+let blocks = {};
+let blocksFlag = {};
+
 module.exports= {
     selectCheck: function(value, selectValue){
         if(value == selectValue){
@@ -6,5 +13,39 @@ module.exports= {
         else{
             return '';
         }
-    }
+    },
+
+    ifCond: function(expression, ops) {
+        let result;
+        let context = this;
+        
+        with(context) {
+            result = (function() {
+                try {
+                    return eval(expression);
+                }
+                catch (e) {
+                    if (e instanceof ReferenceError) {
+                        return false;
+                    }
+                    else {
+                        console.warn('Expression: {{ifCond \'' + expression + '\'}}\nJS-Error: ', e, '\nContext: ', context);
+                    }
+                }
+            }).call(context);
+        }
+
+        return result ? ops.fn(this) : ops.inverse(this);
+    },
+
+    partialsDirs: function(src) {     // Handlebars return partials and all folders in partials as array
+        let partialsDir = fs.readdirSync(src).filter(f => fs.lstatSync(path.join(src, f)).isDirectory());
+        partialsDir.push(path.join(src));
+
+        for (let i = 0, n = partialsDir.length; i < n - 1; i++) {
+            partialsDir[i] = path.join(src, partialsDir[i]);
+        }
+
+        return partialsDir;
+    },
 }

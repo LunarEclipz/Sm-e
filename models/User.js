1e57dee0cdf1adeff41765f77a9b1fb3903369a4
Aadi <44631042+AadiRawr@users.noreply.github.com>
2019-08-05 01:41:58 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,15 +6,56 @@ Note that Sequelize automatically pleuralizes the entity name as the table name
 */
 
 const User = db.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
     name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allownull: false,
+        unique: true
     },
+
+    fname: {
+        type: Sequelize.STRING,
+        allownull: false,
+        unique: true
+    },
+
+    lname: {
+        type: Sequelize.STRING,
+        allownull: false,
+        unique: true
+    },
+
     email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
     },
+
     password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    acctype: {
         type: Sequelize.STRING
     },
+
+    dob: {
+        type: Sequelize.STRING
+    },
+
+    number: {
+        type: Sequelize.STRING
+    },
+
+    
+
 });
 
 module.exports = User;
